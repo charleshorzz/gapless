6e343362e70cd382f30f3f39bebfd003f870650d
TAN PO HUI <phxx04@gmail.com>
2025-03-06 23:05:56 +0800

    / Chat page / Connet wallet modify

--- packages/nextjs/app/(pages)/_components/AuthBackground.tsx
+++ packages/nextjs/app/(pages)/_components/AuthBackground.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const AuthBackground = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex justify-center items-center h-full bg-indigo-100 rounded-md ">{children}</div>;
+  return (
+    <div className="flex justify-center items-center h-full  rounded-lg  border-neutral-200 dark:border-neutral-700 p-3 shadow-sm  bg-gradient-to-r from-blue-50 to-purple-50 dark:from-neutral-700 dark:to-neutral-800 ">
+      {children}
+    </div>
+  );
 };
 
 export default AuthBackground;
--- packages/nextjs/app/(pages)/chats/page.tsx
+++ packages/nextjs/app/(pages)/chats/page.tsx
@@ -37,6 +37,24 @@ const ChatsPage = () => {
                     <button className="btn btn-primary btn-sm" onClick={openConnectModal} type="button">
                       Connect Wallet
                     </button>
+                    {/* <button className="btn btn-warning bg-white text-black border-[#e5e5e5] rounded-md">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="icon icon-tabler icons-tabler-outline icon-tabler-brand-gitlab"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M21 14l-9 7l-9 -7l3 -11l3 7h6l3 -7z" />
+                      </svg>
+                      Connect Wallet
+                    </button> */}
                   </AuthBackground>
                 );
               }
