7f308dc1729d269753906c889eb348cfd5af0605
charleshorzz <horyuanli43@gmail.com>
2025-03-07 15:21:54 +0800

    fix layout.tsx hydrationError and add page.tsx

---
+++ packages/nextjs/app/ScaffoldEthApp.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { SideBar } from "./SideBar";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "@rainbow-me/rainbowkit/styles.css";
+import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
+import { SwitchTheme } from "~~/components/SwitchTheme";
+import { ThemeProvider } from "~~/components/ThemeProvider";
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import "~~/styles/globals.css";
+
+const graphqlEndpoint = "https://api.studio.thegraph.com/query/105777/gapless/version/latest";
+
+const client = new ApolloClient({
+  uri: graphqlEndpoint,
+  cache: new InMemoryCache(),
+});
+
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider enableSystem>
+        <ScaffoldEthAppWithProviders>
+          <SideBar>
+            {/* Top-right buttons */}
+            <div className="absolute top-4 right-4 flex items-center space-x-4">
+              <SwitchTheme />
+              <RainbowKitCustomConnectButton />
+            </div>
+
+            {/* Main Content */}
+            <div className="flex flex-1">
+              <div className="p-2 md:p-4 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-slate-50 dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+                {children}
+              </div>
+            </div>
+          </SideBar>
+        </ScaffoldEthAppWithProviders>
+      </ThemeProvider>
+    </ApolloProvider>
+  );
+};
+
+export default ScaffoldEthApp;
---
+++ packages/nextjs/utils/inputEnums.ts
@@ -0,0 +1,21 @@
+export const GENDER_OPTIONS = ["Male", "Female", "PreferNotToSay"] as const;
+export const ETHNICITY_OPTIONS = ["Asian", "Black", "White", "Hispanic", "Other"] as const;
+export const EDUCATION_OPTIONS = [
+  "Less than High School",
+  "High School",
+  "Diploma",
+  "Bachelor's Degree",
+  "Master's Degree",
+  "Doctoral's Degree",
+  "Professional's Degree",
+] as const;
+export const SENIORITY_OPTIONS = ["Entry", "Associate", "Senior", "Manager", "Director", "VP", "Others"] as const;
+export const EXPERIENCE_OPTIONS = [
+  "Less than one year",
+  "1 year",
+  "2-3 years",
+  "4-5 years",
+  "6-7 years",
+  "8-9 years",
+  "More than 10 years",
+] as const;
