102c0cd3277ca695f2ebd420670a7b24349cae03
TAN PO HUI <phxx04@gmail.com>
2025-03-08 03:40:42 +0800

    undo infinite scroll

--- packages/nextjs/app/ScaffoldEthApp.tsx
+++ packages/nextjs/app/ScaffoldEthApp.tsx
@@ -24,7 +24,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
           <SideBar>
             {/* Main Content */}
             <div className="flex flex-1">
-              <div className="p-2 md:p-4 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-slate-50 dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+              <div className="p-2 md:p-4 rounded-tl-2xl border-b-0 border border-neutral-200 dark:border-neutral-700 bg-slate-50 dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
                 {children}
               </div>
             </div>
