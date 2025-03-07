16a7346efb4ab50ef67958d0c026d22d12dc20cb
charleshorzz <horyuanli43@gmail.com>
2025-03-07 10:42:17 +0800

    add graphql init with example usage in post/page.tsx

--- packages/nextjs/app/(pages)/posts/page.tsx
+++ packages/nextjs/app/(pages)/posts/page.tsx
@@ -3,7 +3,9 @@
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { useAccount } from "wagmi";
+import { Breadcumb } from "~~/components/ui/breadcumb";
 import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
+import { usePostCreatedsQuery } from "~~/libs/generated/graphql";
 import { fetchDataFromIPFS, uploadFileToIPFS } from "~~/utils/pinata";
 
 type FormData = {
@@ -15,12 +17,35 @@ type FormData = {
 };
 
 const PostsPage = () => {
+  //STATE
   const { handleSubmit, control, register } = useForm<FormData>();
   const { writeContractAsync: writePostContractAsync } = useScaffoldWriteContract({ contractName: "PostContract" });
   const { address: userAddress } = useAccount();
-
+  const [postData, setPostData] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
 
+  //API
+  const {
+    data,
+    loading: queryLoading,
+    error,
+  } = usePostCreatedsQuery({
+    variables: { first: 10 },
+    fetchPolicy: "network-only",
+    notifyOnNetworkStatusChange: true,
+    onCompleted: data => {
+      console.log("GraphQL Response:", data);
+      if (data?.postCreateds?.length > 0) {
+        setPostData(data.postCreateds);
+      } else {
+        console.warn("No posts found!");
+      }
+    },
+  });
+
+  console.log("postData state:", postData);
+
+  //FUNCTION
   const onSubmit = async (data: FormData) => {
     try {
       setLoading(true);
@@ -45,77 +70,60 @@ const PostsPage = () => {
   };
 
   return (
-    <div className="text-center mt-8 bg-primary p-10">
-      <h1 className="text-4xl my-0">Create a Post</h1>
-
+    <div className=" mt-8  w-full">
+      <Breadcumb />
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 w-full max-w-md mx-auto mt-8">
-        {/* Name Input */}
-        <fieldset className="fieldset">
-          <legend className="fieldset-legend">What is your name?</legend>
-          <input {...register("name", { required: true })} type="text" className="input" placeholder="Type here" />
-        </fieldset>
-
-        {/* Age Input */}
-        <fieldset className="fieldset">
-          <legend className="fieldset-legend">What is your age?</legend>
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter Your Age</label>
           <input
-            {...register("age", { required: true, valueAsNumber: true })}
             type="number"
-            className="input"
-            placeholder="Enter age"
+            id="age"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter your age"
+            required
           />
-        </fieldset>
+        </div>
 
-        {/* Gender Dropdown */}
-        <fieldset className="fieldset">
-          <legend className="fieldset-legend">Gender</legend>
-          <Controller
-            control={control}
-            name="gender"
-            render={({ field }) => (
-              <select {...field} className="select">
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-                <option value="PreferNotToSay">Prefer Not To Say</option>
-              </select>
-            )}
-          />
-        </fieldset>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Your Gender</label>
+        <select
+          id="gender"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option selected>Choose Your Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Others">Others</option>
+        </select>
 
-        {/* Ethnicity Dropdown */}
-        <fieldset className="fieldset">
-          <legend className="fieldset-legend">Ethnicity</legend>
-          <Controller
-            control={control}
-            name="ethnicity"
-            render={({ field }) => (
-              <select {...field} className="select">
-                <option value="Asian">Asian</option>
-                <option value="Black">Black</option>
-                <option value="White">White</option>
-                <option value="Hispanic">Hispanic</option>
-                <option value="Other">Other</option>
-              </select>
-            )}
-          />
-        </fieldset>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Your Ethnicity</label>
+        <select
+          id="ethnicity"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option selected>Choose Your Ethnicity</option>
+          <option value="Asian">Asian</option>
+          <option value="White">White</option>
+          <option value="Hispanic">Hispanic</option>
+          <option value="Black">Black</option>
+          <option value="Indian">Others</option>
+        </select>
 
-        {/* Highest Education Dropdown */}
-        <fieldset className="fieldset">
-          <legend className="fieldset-legend">Highest Education</legend>
-          <Controller
-            control={control}
-            name="education"
-            render={({ field }) => (
-              <select {...field} className="select">
-                <option value="High School">High School</option>
-                <option value="Bachelor's">Bachelors</option>
-                <option value="Master's">Masters</option>
-                <option value="PhD">PhD</option>
-              </select>
-            )}
-          />
-        </fieldset>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Select Your Highest Education
+        </label>
+        <select
+          id="education"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option selected>Choose Your Highest Education</option>
+          <option value="Less than High School">Less than High School</option>
+          <option value="High School">High School</option>
+          <option value="Diploma">Diploma</option>
+          <option value="Bachelor's Degree">Bachelor Degree</option>
+          <option value="Master's Degree">Master Degree</option>
+          <option value="Doctoral's Degree">Doctoral Degree (Phd, EdD, etc)</option>
+          <option value="Professional's Degree">Professional Degree (MD, JD, DDS, etc)</option>
+        </select>
 
         {/* Submit Button */}
         <button type="submit" className="btn btn-primary mt-4" disabled={loading}>
